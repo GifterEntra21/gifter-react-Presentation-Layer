430b0f4bad30a8a6eaa2c012fef0d367bd1ba979
Ana Paula <aa612691@gmail.com>
2022-08-17 14:39:00 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Card from './Components/Card.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+    return (
+        <div classname='app'>
+    <Card title='Luminaria One Piece' 
+    imageurl='https://m.media-amazon.com/images/I/81fvNmmrd3L._AC_SX522_.jpg' 
+    body='Luminaria LED 3D, com 7 cores. Inclui: Cabo USB, uma base e uma luminária.'
+    link="https://shopee.com.br/product/570256376/16881347658?af_click_lookback=7d&af_reengagement_window=7d&af_siteid=an_18112960000&af_sub_siteid=XoWTi8WRo1A-5WJIYCieteUTZy.3Dco.BQ&af_viewthrough_lookback=1d&c=-&is_retargeting=true&pid=affiliates&utm_campaign=-&utm_content=XoWTi8WRo1A-5WJIYCieteUTZy.3Dco.BQ&utm_medium=affiliates&utm_source=an_18112960000&utm_term=7q1arjw3gkb9XoWTi8WRo1A-5WJIYCieteUTZy.3Dco.BQ" />
+    
+    <Card title='Suplemento BIOMAXX' 
+    imageurl='https://melcoprol.com.br/images/produtos/Biomaxx-60caps-550mg.jpg' 
+    body='Suplemento para usar na academia.' />
+    
+    <Card title='Bonecos Genshin Impact' 
+    imageurl='https://cf.shopee.com.br/file/dff3fda1eca63b45077cc3dbc809781e' 
+    body='Bonecos de pelúcia Genshin Impact - Coleção Completa' />
+        </div>
+    )
 }
 
 export default App;
